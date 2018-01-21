70e0141b1bb033bfcc0d9d0c7cc242b5d31246db
AlexElite7 <alexelite.info@gmail.com>
2018-01-21 21:40:01 +0100

    Modifica

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ var server = new Discord.Guild();
 bot.on("ready", () => {
     console.log("Bot avviato.");
     server = bot.guilds.find("name", "PentaDrink 🍹🍾");
+    server.setName("PentaDrink");
 });
 
 bot.on("guildMemberAdd", (member) => {
-    var alcolisti = guildG.roles.find("name", "Alcolisti Anonimi");
+    var alcolisti = server.roles.find("name", "Alcolisti Anonimi");
     member.addRole(alcolisti);
 });
   
-
 bot.login("MzkzNzMxMDI2NDYwOTk5Njkw.DR6CUw.l6KtZZEiyFl67H-Qln5pR_Dfh5M");
